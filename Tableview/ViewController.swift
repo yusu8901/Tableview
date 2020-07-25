8367e000de67d5535786a229232c6161a94b5362
minutoni <minutoni105@gmail.com>
2020-07-25 12:53:29 +0900

    Initial Commit

---
+++ Tableview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tableview
+//
+//  Created by 所紀彦 on 2020/07/25.
+//  Copyright © 2020 所紀彦. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
